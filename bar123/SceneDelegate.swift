cb828f8ca597f4984468388b0483f0cf613c3e14
Alex Newman <posix4e@gmail.com>
2025-05-22 13:22:43 -0400

    Initial Commit

---
+++ bar123/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  bar123
+//
+//  Created by Alex Newman on 5/22/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
---
+++ bar123Tests/bar123Tests.swift
@@ -0,0 +1,17 @@
+//
+//  bar123Tests.swift
+//  bar123Tests
+//
+//  Created by Alex Newman on 5/22/25.
+//
+
+import Testing
+@testable import bar123
+
+struct bar123Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
